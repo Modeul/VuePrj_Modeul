8f975761761e407e0bd36b0a41e86c9fca7172a7
sungmin <kimmoksil@gmail.com>
2023-04-10 12:00:18 +0900

    [feat] detail.vue deadline 날짜 formatting

--- src/components/member/stuff/Detail.vue
+++ src/components/member/stuff/Detail.vue
@@ -1,148 +1,151 @@
 <script>
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko'
+
 export default {
-  data() {
-    return {
-      openModal: false,
-      stuff: {},
-      category: {},
-      image: {},
-    };
-  },
-  methods: {
-    /* 모달 이벤트 */
-    modalHandler() {
-      this.openModal = !this.openModal;
-    },
-    imageZoomInHandler() {
-      console.log("zoom-in");
-    },
-    imageZoomOutHandler() {
-      console.log("zoom-out");
-    },
-  },
-  mounted() {
-    // fetch(`http://localhost:8080/member/stuffs/${this.$route.params.id}`)
-    //     .then(response => response.json())
-    //     .then(stuff => this.stuff = stuff)
-    //     .catch(error => console.log("error", error));
+	data() {
+		return {
+			openModal: false,
+			stuff: {},
+			category: {},
+			image: {},
+		};
+	},
+	methods: {
+		/* 모달 이벤트 */
+		modalHandler() {
+			this.openModal = !this.openModal;
+		},
+		imageZoomInHandler() {
+			console.log("zoom-in");
+		},
+		imageZoomOutHandler() {
+			console.log("zoom-out");
+		},
+		formatDateStuff() {
+			const deadlineObj = dayjs(this.stuff.deadline).locale('ko');
+			this.stuff.deadline = deadlineObj.format("M월 D일 (dd) HH시까지");
+		}
+	},
+	mounted() {
+		// fetch(`http://localhost:8080/member/stuffs/${this.$route.params.id}`)
+		//     .then(response => response.json())
+		//     .then(stuff => this.stuff = stuff)
+		//     .catch(error => console.log("error", error));
 
-    fetch(`http://localhost:8080/member/stuffs/${this.$route.params.id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.stuff = data.stuff;
-        this.category = data.category;
-        this.image = data.image;
-      })
-      .catch((error) => console.log("error", error));
+		fetch(`http://localhost:8080/member/stuffs/${this.$route.params.id}`)
+			.then((response) => response.json())
+			.then((data) => {
+				this.stuff = data.stuff;
+				this.category = data.category;
+				this.image = data.image;
+				this.formatDateStuff();
+			})
+			.catch((error) => console.log("error", error));
 
-    // this.imageZoomInHandler();
-    // this.imageZoomOutHandler();
-  },
-  updated() {
-    console.log(this.stuff);
-    console.log(this.image);
-    console.log(this.category);
-  },
+		// this.imageZoomInHandler();
+		// this.imageZoomOutHandler();
+	},
+	updated() {
+		console.log(this.stuff);
+		console.log(this.image);
+		console.log(this.category);
+	},
 };
 </script>
 
 <style scoped></style>
 
 <template>
-  <!-- detail : flex-container -->
-  <div class="detail">
-    <header>
-      <router-link router-link to="list" class="icon icon-back"
-        >뒤로가기</router-link
-      >
-    </header>
+	<!-- detail : flex-container -->
+	<div class="detail">
+		<header>
+			<router-link router-link to="list" class="icon icon-back">뒤로가기</router-link>
+		</header>
 
-    <!-- detail - item1  -->
-    <main>
-      <!-- detail-main : flex-container -->
-      <div class="detail-main">
-        <!-- detail-img : detail-main - item1 -->
-        <div class="detail-img">
-          <!-- <img src="../../../../images/member/stuff/chick.jpg" alt="img"> -->
-          <img :src="'/images/member/stuff/' + image.name" alt="img" />
+		<!-- detail - item1  -->
+		<main>
+			<!-- detail-main : flex-container -->
+			<div class="detail-main">
+				<!-- detail-img : detail-main - item1 -->
+				<div class="detail-img">
+					<!-- <img src="../../../../images/member/stuff/chick.jpg" alt="img"> -->
+					<img :src="'/images/member/stuff/' + image.name" alt="img" />
 
-          <!-- image : modal -->
-          <div class="detail-modal">
-            <div
-              class="icon-close"
-              src="../../images/member/stuff/heart.png"
-            ></div>
-            <!-- <img class="icon-close" src="../../images/member/stuff/heart.png"> -->
-            <img class="modal-img" />
-          </div>
+					<!-- image : modal -->
+					<div class="detail-modal">
+						<div class="icon-close" src="../../images/member/stuff/heart.png"></div>
+						<!-- <img class="icon-close" src="../../images/member/stuff/heart.png"> -->
+						<img class="modal-img" />
+					</div>
 
-          <!-- image : slideshow -->
-        </div>
-        <!-- detail-heading : detail-main - item2 -->
-        <section class="canvas detail-heading">
-          <h1 class="d-none">heading</h1>
-          <div class="detail-category">{{ category.name }}</div>
-          <p class="detail-heading-title">{{ stuff.title }}</p>
-          <div class="d-fl">
-            <div class="detail-price">{{ stuff.price }}</div>
-            <div class="detail-status">모집중</div>
-            <div class="detail-heart">찜</div>
-            <div class="icon-heart">하트</div>
-          </div>
-        </section>
-        <!-- detail-info : detail-main - item3 -->
-        <section class="canvas detail-info">
-          <h1 class="d-none">info</h1>
-          <div>•인원</div>
-          <div>2 / {{ stuff.numPeople }}</div>
-          <div>•기한</div>
-          <div>{{ stuff.deadline }}</div>
-          <div>•장소</div>
-          <div>{{ stuff.place }}</div>
-        </section>
-        <!-- detail-writing : detail-main - item4 -->
-        <section class="canvas detail-writing">
-          <h1 class="d-none">writing</h1>
-          <p class="detail-paragraph">
-            {{ stuff.content }}
-          </p>
-        </section>
-      </div>
-    </main>
+					<!-- image : slideshow -->
+				</div>
+				<!-- detail-heading : detail-main - item2 -->
+				<section class="canvas detail-heading">
+					<h1 class="d-none">heading</h1>
+					<div class="detail-category">{{ category.name }}</div>
+					<p class="detail-heading-title">{{ stuff.title }}</p>
+					<div class="d-fl">
+						<div class="detail-price">{{ stuff.price }}</div>
+						<div class="detail-status">모집중</div>
+						<div class="detail-heart">찜</div>
+						<div class="icon-heart">하트</div>
+					</div>
+				</section>
+				<!-- detail-info : detail-main - item3 -->
+				<section class="canvas detail-info">
+					<h1 class="d-none">info</h1>
+					<div>•인원</div>
+					<div>2 / {{ stuff.numPeople }}</div>
+					<div>•기한</div>
+					<div>{{ stuff.deadline }}</div>
+					<div>•장소</div>
+					<div>{{ stuff.place }}</div>
+				</section>
+				<!-- detail-writing : detail-main - item4 -->
+				<section class="canvas detail-writing">
+					<h1 class="d-none">writing</h1>
+					<p class="detail-paragraph">
+						{{ stuff.content }}
+					</p>
+				</section>
+			</div>
+		</main>
 
-    <!-- detail-join : detail - itme2  -->
+		<!-- detail-join : detail - itme2  -->
 
-    <section class="canvas detail-join">
-      <h1 class="d-none">join</h1>
-      <h2 class="detail-join-title">참여중인 사람</h2>
-      <div class="detail-join-members">
-        <a class="icon-member">멤버a</a>
-        <a class="icon-member">멤버b</a>
-        <a class="icon-member">멤버c</a>
-        <a class="icon-member">멤버d</a>
-        <a class="icon-member">멤버e</a>
-      </div>
-      <button class="detail-join-button" onclick="location.href='list.html'">
-        참여하기
-      </button>
-    </section>
+		<section class="canvas detail-join">
+			<h1 class="d-none">join</h1>
+			<h2 class="detail-join-title">참여중인 사람</h2>
+			<div class="detail-join-members">
+				<a class="icon-member">멤버a</a>
+				<a class="icon-member">멤버b</a>
+				<a class="icon-member">멤버c</a>
+				<a class="icon-member">멤버d</a>
+				<a class="icon-member">멤버e</a>
+			</div>
+			<button class="detail-join-button" onclick="location.href='list.html'">
+				참여하기
+			</button>
+		</section>
 
-    <!-- 모달버튼 -->
-    <div>
-      <div @click="openModal = true" class="btn-default">수정</div>
-      <div @click="openModal = true" class="btn-default">삭제</div>
-    </div>
-  </div>
+		<!-- 모달버튼 -->
+		<div>
+			<div @click="openModal = true" class="btn-default">수정</div>
+			<div @click="openModal = true" class="btn-default">삭제</div>
+		</div>
+	</div>
 
-  <!-- 수정/삭제 모달 -->
-  <div class="black-bg" v-if="openModal == true">
-    <div class="white-bg">
-      <h1>모달 테스트</h1>
-      <p>수정</p>
-      <p>삭제</p>
-      <button class="close-button" @click.prevent="modalHandler">닫기</button>
-    </div>
-  </div>
+	<!-- 수정/삭제 모달 -->
+	<div class="black-bg" v-if="openModal == true">
+		<div class="white-bg">
+			<h1>모달 테스트</h1>
+			<p>수정</p>
+			<p>삭제</p>
+			<button class="close-button" @click.prevent="modalHandler">닫기</button>
+		</div>
+	</div>
 </template>
 
 <style scoped>
