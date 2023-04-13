8e84e2d0847b51378a2f28933c703c142a38b263
minkyungkimm <dusenalsrud@naver.com>
2023-04-13 17:58:48 +0900

    [fix] listsearch list css 수정

--- src/components/member/stuff/Listsearch.vue
+++ src/components/member/stuff/Listsearch.vue
@@ -20,25 +20,15 @@ export default {
 			this.page = 1;
 			e.preventDefault();
             this.query = e.target.value;
-			this.queryisVal = false;
+	
 			console.log(this.query);
 			fetch(`http://localhost:8080/member/stuffs?p=${this.page}&q=${this.query}`)
 				.then(response => response.json())
 				.then(dataList => {
-					this.queryList = this.formatDateList(dataList.queryList);
-					console.log(this.queryList);
+					this.list = this.formatDateList(dataList.queryList);
+				
 				}).catch(error => console.log('error', error));
         },
-		// categoryHandler(e){
-		// 	this.categoryId = e.target.value;
-		// 	console.log(this.categoryId);
-		// 	fetch(`http://localhost:8080/member/stuffs?c=${this.categoryId}`)
-		// 		.then(response => response.json())
-		// 		.then(dataList => {
-		// 			this.list = this.formatDateList(dataList.list);
-		// 			this.categoryList = dataList.categoryList;
-		// 		}).catch(error => console.log('error', error));
-		// },
 		addListHandler() {
 			this.page++;
 			fetch(`http://localhost:8080/member/stuffs?q=${this.query}&p=${this.page}&c=${this.categoryId}`)
@@ -51,20 +41,53 @@ export default {
 				})
 				.catch(error => console.log('error', error));
 		},
-		formatDateList(list) {
-			if(list.length == 0)
+        formatDateList(list) {
+			if (list.length == 0)
 				return;
 			let resultList = [];
 			for (let item of list) {
-				if(item.deadline == null)
+				if (item.deadline == null)
 					continue;
+				const today = new dayjs().format('YYYY-MM-DD');
+
 				const deadlineObj = dayjs(item.deadline).locale('ko');
-				
-				item.deadline = deadlineObj.format("M월 D일 (dd) HH시까지");
+				const isToday = (deadlineObj.format('YYYY-MM-DD') == today) ? '오늘, ' : ''
+				item.deadline = isToday + deadlineObj.format("M월 D일 (dd) HH시까지");
+
+				// deadlineState
+				// 0: 마감 -> (마감) // 회색
+				// 1: 1일 이상 남음 -> (D-n) // 파랑? 초록?
+				// 2: 당일 마감 -> (마감 n 시간 전) // 초록? 주황? 
+				// 3: 1시간 내 마감 -> (1시간 내 마감)  // 빨강?
+
+				item.dDay = dayjs().diff(deadlineObj, 'day');
+				if (parseInt(item.dDay) < 0){
+					item.dDay = 'D' + item.dDay;
+					item.deadlineState = 1;
+				}
+				else if (parseInt(item.dDay) == 0) {
+					item.dDay = deadlineObj.diff(dayjs(), 'hours')
+					if (parseInt(item.dDay) > 0){
+						item.dDay = '마감 ' + deadlineObj.diff(dayjs(), 'hours') + '시간 전'
+						item.deadlineState = 2;
+					}
+					else if (parseInt(item.dDay) == 0){
+						item.dDay = '1시간 내 마감';
+						item.deadlineState = 3;
+					}
+					else{
+						item.dDay = '마감';
+						item.deadlineState = 0;
+					}
+				}
+				else{
+					item.dDay = '마감';
+					item.deadlineState = 0;
+				}
 				resultList.push(item);
 			}
 			return resultList;
-		},
+        }
 	},
 	mounted() {
 		this.page = 0;
@@ -74,13 +97,14 @@ export default {
 </script>
 <style scoped>
 @import url(/css/component/member/stuff/component-list.css);
+@import url(/css/component/member/stuff/component-list-search.css);
 </style>
 <template>
     <section class="canvas b-rad-2">
 
         <!-- 검색창 들어가는 부분 -->
         <header class="d-fl-al">
-            <div>
+                <div>
                     <router-link to="/member/stuff/list" class="icon icon-back">뒤로가기</router-link>
                 </div>
 
@@ -92,64 +116,44 @@ export default {
                 </div>
         </header>
 
-    <!-- 검색창 들어가는 부분 -->
-
-       <!-- <nav>
-			<div class="header-categ-box" v-if="queryisVal">
-				<div>
-					<button class="header-categ" @click="categoryHandler" name="c">전체</button>
-				</div>
-				
-				<div v-for="c in categoryList">
-					<button class="header-categ" @click="categoryHandler" :value="c.id">{{ c.name }}</button>
-				</div>
-			</div>
-		</nav> -->
-
        <main>
-		<div v-if="queryisVal">
+        
 			<div class="stuff-list" v-for="stuff in list">
 				<router-link :to="'./' + stuff.id">
-					<div class="d-gr li-gr m-t-13px list-cl">
-						<div class="li-pic b-rad-1">
-							<img class="listview-image" :src="'/images/member/stuff/' + stuff.imageName" alt="img">
+						<div class="d-gr li-gr m-t-13px list-cl">
+							<div class="li-pic b-rad-1">
+								<img v-if="stuff.imageName != null" class="listview-image" :src="'/images/member/stuff/' + stuff.imageName" alt="img">
+								<img v-else-if="stuff.categoryId == '1'" class="listview-image" src="/images/member/stuff/category1.png" alt="img">
+								<img v-else-if="stuff.categoryId == '2'" class="listview-image" src="/images/member/stuff/category2.png" alt="img">
+								<img v-else-if="stuff.categoryId == '3'" class="listview-image" src="/images/member/stuff/category3.png" alt="img">
+								<img v-else class="listview-image" src="/images/member/stuff/member.png" alt="img">
+							</div>
+							<div class="li-categ-place">
+								<span class="li-categ-place-categoryName">
+									{{ stuff.categoryName }}
+								</span>
+								<span class="li-categ-place-p">
+									{{ stuff.place }}
+								</span>
+							</div>
+							<div class="li-dday"
+							:class="(stuff.deadlineState == 0)? 'expired' : 
+							(stuff.deadlineState == 1)? 'day-left' : 
+							(stuff.deadlineState == 2)? 'hour-left' : 'minute-left' ">{{ stuff.dDay }}</div>
+							<div class="li-subj">{{ stuff.title }}</div>
+							<div class="li-member">
+								<span class="li-member-limit"> 1</span>
+								/ {{ stuff.numPeople }} 명
+							</div>
+							
 						</div>
-						<div class="li-categ header-categ li-header-categ">{{ stuff.categoryName }}</div>
-						<div class="li-subj">{{ stuff.title }}</div>
-						<div class="li-member"> 1 / {{ stuff.numPeople }}</div>
-						<div class="li-date">{{ stuff.deadline }}</div>
-					</div>
-					<div>
-						<h1 class="icon icon-line">선 긋기</h1>
-					</div>
-				</router-link>
+					</router-link>
 			</div>
-		</div>
-		<div v-if="!queryisVal">
-			<div class="stuff-list" v-for="stuff in queryList">
-				<router-link :to="'./' + stuff.id">
-					<div class="d-gr li-gr m-t-13px list-cl">
-						<!-- 나중에 전체를 div로 묶어서 main으로 크게 묶기 -->
-						<div class="li-pic b-rad-1">
-							<img class="listview-image" :src="'/images/member/stuff/' + stuff.imageName" alt="img">
-						</div>
-						<div class="li-categ header-categ li-header-categ">{{ stuff.categoryName }}</div>
-						<div class="li-subj">{{ stuff.title }}</div>
-						<div class="li-member"> 1 / {{ stuff.numPeople }}</div>
-						<div class="li-date">{{ stuff.deadline }}</div>
-					</div>
-					<div>
-						<h1 class="icon icon-line">선 긋기</h1>
-					</div>
-				</router-link>
-			</div>
-		</div>
+		
 
-
-			<button class="btn-next more-list" @click="addListHandler">더보기</button>
+			<!-- <button class="btn-next more-list" @click="addListHandler">더보기</button> -->
 			<router-link to="/member/stuff/reg">
 				<div class="reg-stuff">
-					+
 				</div>
 			</router-link>
 		</main>
@@ -158,21 +162,3 @@ export default {
     </section>
 </template>
 
-
-<style scoped>
-    @import "/css/component/member/stuff/component-list-search.css";
-	
-.reg-stuff {
-	width: 30px;
-	height: 30px;
-	background-color: #63A0C2;
-	color: #fff;
-	border-radius: 50%;
-	text-align: center;
-	line-height: 30px;
-	position: fixed;
-	right: 30px;
-	bottom: 30px;
-	cursor: pointer;
-}
-</style>
